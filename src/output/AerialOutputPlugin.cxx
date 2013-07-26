a84d5b7b16419f599203ccae02a28b6ba958a7e6
Aaron Griffith <aargri@gmail.com>
2013-07-26 16:46:42 -0400

    output/aerial: asynchronous messaging and errors

--- src/output/AerialOutputPlugin.cxx
+++ src/output/AerialOutputPlugin.cxx
@@ -32,6 +32,7 @@ struct AerialOutput {
 
 	const char *host;
 	
+	GError *async_error;
 	AerialClient *client;
 };
 
@@ -44,6 +45,8 @@ aerial_output_quark(void)
 static struct audio_output *
 aerial_output_init(const config_param *param, GError **error_r)
 {
+	g_type_init();
+	
 	AerialOutput *ad = new AerialOutput();
 	if (!ao_base_init(&ad->base, &aerial_output_plugin, param, error_r)) {
 		delete ad;
@@ -52,6 +55,7 @@ aerial_output_init(const config_param *param, GError **error_r)
 	
 	ad->host = config_get_block_string(param, "host", "localhost");
 	ad->client = nullptr;
+	ad->async_error = nullptr;
 	
 	return &ad->base;
 }
@@ -60,6 +64,9 @@ static void
 aerial_output_finish(struct audio_output *ao)
 {
 	AerialOutput *ad = (AerialOutput *)ao;
+	if (ad->async_error)
+		g_error_free(ad->async_error);
+	ad->async_error = nullptr;
 	
 	ao_base_finish(&ad->base);
 	delete ad;
@@ -69,7 +76,33 @@ static void
 aerial_output_close(struct audio_output *ao)
 {
 	AerialOutput *ad = (AerialOutput *)ao;
-	aerial_client_disconnect_from_host(ad->client, NULL);
+	aerial_client_disconnect_from_host_async(ad->client, NULL, NULL);
+	g_object_unref(ad->client);
+	ad->client = nullptr;
+}
+
+static void
+aerial_on_error(GObject *src, GError *err, AerialOutput *ad)
+{
+	if (ad->async_error == nullptr) {
+		ad->async_error = g_error_copy(err);
+	}
+}
+
+static void
+aerial_on_play(GObject *src, GAsyncResult *res, gpointer data)
+{
+	AerialClient *ac = AERIAL_CLIENT(src);
+	bool success = aerial_client_play_finish(ac, res, NULL);
+}
+
+static void
+aerial_on_connect(GObject *src, GAsyncResult *res, gpointer data)
+{
+	AerialClient *ac = AERIAL_CLIENT(src);
+	bool success = aerial_client_connect_to_host_finish(ac, res, NULL);
+	if (success)
+		aerial_client_play_async(ac, aerial_on_play, data);
 }
 
 static bool
@@ -77,6 +110,12 @@ aerial_output_open(struct audio_output *ao, struct audio_format *audio_format,
 	       GError **error)
 {
 	AerialOutput *ad = (AerialOutput *)ao;
+
+	if (ad->async_error) {
+		g_propagate_error(error, ad->async_error);
+		ad->async_error = nullptr;
+		return false;
+	}
 	
 	// our output format is fixed
 	audio_format->format = SAMPLE_FORMAT_S16;
@@ -84,12 +123,25 @@ aerial_output_open(struct audio_output *ao, struct audio_format *audio_format,
 	audio_format->channels = 2;
 
 	ad->client = aerial_client_new();
-	if (!aerial_client_connect_to_host(ad->client, ad->host, error))
-		return false;
-	if (!aerial_client_play(ad->client, error))
-		return false;
+	g_signal_connect(ad->client, "on-error", G_CALLBACK(aerial_on_error), ad);
+	aerial_client_connect_to_host_async(ad->client, ad->host, aerial_on_connect, ao);
 
 	return true;
+}
+
+static unsigned
+aerial_output_delay(struct audio_output *ao)
+{
+	AerialOutput *ad = (AerialOutput *)ao;
+	
+	if (ad->async_error)
+		return 0;
+	
+	if (aerial_client_get_state(ad->client) != AERIAL_CLIENT_STATE_PLAYING)
+		return 100;
+	if (aerial_client_get_write_space(ad->client) < AERIAL_CLIENT_BYTES_PER_FRAME)
+		return aerial_client_get_local_buffer_length(ad->client) / 10;
+	return 0;
 }
 
 static size_t
@@ -97,14 +149,19 @@ aerial_output_play(struct audio_output *ao, const void *chunk, size_t size,
 				   GError **error)
 {
 	AerialOutput *ad = (AerialOutput *)ao;
-
-	if (aerial_client_get_state(ad->client) != AERIAL_CLIENT_STATE_PLAYING) {
-		if (!aerial_client_play(ad->client, error))
-			return 0;
+	
+	if (ad->async_error) {
+		g_propagate_error(error, ad->async_error);
+		ad->async_error = nullptr;
+		return 0;
 	}
 	
 	guint8 *buffer = reinterpret_cast<guint8*>(const_cast<void*>(chunk));
-	return aerial_client_write(ad->client, buffer, size, NULL);
+	size_t written = aerial_client_write(ad->client, buffer, size, NULL);
+	
+	if (written == 0)
+		g_set_error(error, aerial_output_quark(), 0, "no more room");
+	return written;
 }
 
 const struct audio_output_plugin aerial_output_plugin = {
@@ -116,7 +173,7 @@ const struct audio_output_plugin aerial_output_plugin = {
 	nullptr,
 	aerial_output_open,
 	aerial_output_close,
-	nullptr,
+	aerial_output_delay,
 	nullptr,
 	aerial_output_play,
 	nullptr,
